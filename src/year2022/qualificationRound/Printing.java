78022b8346569ab4fa8ff5543bf75b0727c63ea7
sushil.or.k <sushil.or.k@gmail.com>
2022-04-02 22:17:03 +0530

    yeear 2022

--- src/year2022/qualificationRound/Printing.java
+++ src/year2022/qualificationRound/Printing.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class PunchedCards {
+public class Printing {
 
     public static void main(String[] args) {
         Scanner scanner = null;
